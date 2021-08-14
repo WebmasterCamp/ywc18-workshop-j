d2c13edec67969528488f67350ddbdf5a636d442
Mild-TN <21-mild-tn@users.noreply.git.innovasive.co.th>
2021-08-14 13:37:19 +0700

    Add main layout

---
+++ core/components/Layouts/Main.tsx
@@ -0,0 +1,29 @@
+import React, { ReactChild, useEffect, useState } from "react";
+
+import { Navbar } from "../Navbar";
+
+interface MainLayoutProps {
+  children: ReactChild;
+  topping?: any;
+  bgImage?: string;
+  bannerContent?: any;
+}
+
+export const MainLayout = (props: MainLayoutProps) => {
+  return (
+    <>
+      <div className="relative z-0 h-content bg-grey-200">
+        <div className="fixed inset-x-0 z-50">
+          <Navbar />
+        </div>
+        <div
+          style={{
+            backgroundImage: "url(https://placeimg.com/900/480/people);",
+          }}
+          className="w-full bg-no-repeat bg-cover h-[400px] tablet:h-760"
+        ></div>
+        <div className="mx-auto max-w-7xl">{props.children}</div>
+      </div>
+    </>
+  );
+};
