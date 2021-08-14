7f7be424efb92ec531414e3fade4acb288d5991f
WhatTheFar <jakpat.m@gmail.com>
2021-08-14 18:43:01 +0700

    feat(nav): slogan

--- core/components/Navbar.tsx
+++ core/components/Navbar.tsx
@@ -9,10 +9,13 @@ export const Navbar = () => {
       <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 mb-3 bg-purple-400">
         <div className="container flex flex-wrap items-center justify-between px-4 mx-auto">
           <div className="relative flex justify-between w-full tablet:w-auto tablet:static tablet:block tablet:justify-start">
-            <div className="inline-block py-2 mr-4 text-sm font-bold leading-relaxed text-white uppercase cursor-pointer whitespace-nowrap">
+            <div className="flex items-center inline-block py-2 mr-4 text-sm font-bold leading-relaxed text-white uppercase cursor-pointer whitespace-nowrap">
               <Link href="/">
                 <img src="/icons/logo.png" />
               </Link>
+              <span className="text-white ml-4">
+                Get drunk and rest with best friends!
+              </span>
             </div>
             <button
               className="block px-3 py-1 text-xl leading-none text-white bg-transparent border border-transparent border-solid rounded outline-none cursor-pointer tablet:hidden focus:outline-none"
