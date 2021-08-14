4474028bedabfdabe452c3bee36516beb2476b43
WhatTheFar <jakpat.m@gmail.com>
2021-08-14 19:05:07 +0700

    feat(meta): title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Maomun</title>
+        <title>Maomun | GET DRUNK AND REST WITH BEST FRIENDS!</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
