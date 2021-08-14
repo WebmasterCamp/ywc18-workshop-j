c8599e13c9292931821265676716c44fbae1cf59
Mild-TN <21-mild-tn@users.noreply.git.innovasive.co.th>
2021-08-14 12:50:52 +0700

    Input component

---
+++ core/components/Input.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { SearchIcon } from "./Icons/Search";
+
+interface InputProps {
+  onChange: any;
+  value: string | number;
+  className?: string;
+  placeholder?: string;
+}
+
+export const Input = (props: InputProps) => {
+  const { value, onChange, className, placeholder } = props;
+  return (
+    <div className={`flex ${className} bg-white`}>
+      <div className="flex items-center justify-center h-full pl-2 mt-3 w-14">
+        <SearchIcon stoke="#AAAAAA" className="block tablet:hidden" />
+      </div>
+
+      <input
+        onChange={onChange}
+        className="w-full focus:outline-none"
+        value={value}
+        placeholder={placeholder}
+      />
+    </div>
+  );
+};
