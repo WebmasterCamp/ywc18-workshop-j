b14644970462faf1e89af31698211e1c10a49176
Mild-TN <21-mild-tn@users.noreply.git.innovasive.co.th>
2021-08-14 13:50:20 +0700

    Add file data of home page

---
+++ features/HomePage/HomePageData.tsx
@@ -0,0 +1,34 @@
+export const HomePageData = [
+  {
+    event_id: 1,
+    event_name: "Hello",
+    date: "วันนี้ 18.00 น.",
+    image: "https://placeimg.com/640/480/any/sepia",
+    description: "Details here Details here Details here Details here",
+    tag: ["Category1", "Category2"],
+  },
+  {
+    event_id: 2,
+    event_name: "Hello 2",
+    date: "วันนี้ 18.00 น.",
+    image: "https://placeimg.com/640/480/any/sepia",
+    description: "Details here Details here Details here Details here",
+    tag: ["Category1", "Category2"],
+  },
+  {
+    event_id: 3,
+    event_name: "Hello 3",
+    date: "วันนี้ 18.00 น.",
+    image: "https://placeimg.com/640/480/any/sepia",
+    description: "Details here Details here Details here Details here",
+    tag: ["Category1", "Category2"],
+  },
+  {
+    event_id: 4,
+    event_name: "Hello 4",
+    date: "วันนี้ 18.00 น.",
+    image: "https://placeimg.com/640/480/any/sepia",
+    description: "Details here Details here Details here Details here",
+    tag: ["Category1", "Category2"],
+  },
+];
