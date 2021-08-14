dfcbe2bdea6aaf9e62b6eb8370f107155cf604f5
WhatTheFar <jakpat.m@gmail.com>
2021-08-14 17:39:05 +0700

    feat(footer): add about us href

--- core/components/Footer.tsx
+++ core/components/Footer.tsx
@@ -17,8 +17,20 @@ export const Footer: NextPage = () => {
       </div>
       <div className="ml-4 text-white">© Maomun 2021. All rights reserved.</div>
       <div className="flex-grow flex items-center justify-end">
-        <FacebookIcon />
-        <span className="ml-4 text-white">เกี่ยวกับเรา</span>
+        <a
+          href="https://www.facebook.com/Maomun-%E0%B9%80%E0%B8%A1%E0%B8%B2%E0%B8%A1%E0%B8%B1%E0%B8%99-100235789046125"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FacebookIcon />
+        </a>
+        <a
+          href="https://www.facebook.com/Maomun-%E0%B9%80%E0%B8%A1%E0%B8%B2%E0%B8%A1%E0%B8%B1%E0%B8%99-100235789046125"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <span className="ml-4 text-white">เกี่ยวกับเรา</span>
+        </a>
       </div>
     </div>
   );
