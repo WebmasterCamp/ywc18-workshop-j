52ffbeac906a2de41b33dc59d2768443acdfd345
Mild-TN <21-mild-tn@users.noreply.git.innovasive.co.th>
2021-08-14 16:33:04 +0700

    Update navabr and add logo

--- core/components/Navbar.tsx
+++ core/components/Navbar.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable @next/next/link-passhref */
 import React from "react";
+import Link from "next/link";
 
 export const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
@@ -7,12 +9,11 @@ export const Navbar = () => {
       <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 mb-3 bg-purple-300">
         <div className="container flex flex-wrap items-center justify-between px-4 mx-auto">
           <div className="relative flex justify-between w-full tablet:w-auto tablet:static tablet:block tablet:justify-start">
-            <a
-              className="inline-block py-2 mr-4 text-sm font-bold leading-relaxed text-white uppercase whitespace-nowrap"
-              href="#pablo"
-            >
-              Logo
-            </a>
+            <div className="inline-block py-2 mr-4 text-sm font-bold leading-relaxed text-white uppercase cursor-pointer whitespace-nowrap">
+              <Link href="/">
+                <img src="/icons/logo.png" />
+              </Link>
+            </div>
             <button
               className="block px-3 py-1 text-xl leading-none text-white bg-transparent border border-transparent border-solid rounded outline-none cursor-pointer tablet:hidden focus:outline-none"
               type="button"
@@ -38,12 +39,11 @@ export const Navbar = () => {
                 </a>
               </li>
               <li className="nav-item">
-                <a
-                  className="flex items-center px-3 py-2 text-xs font-bold leading-snug text-white uppercase hover:opacity-75"
-                  href="#pablo"
-                >
-                  <span className="ml-2 text-2xl">เข้าห้อง</span>
-                </a>
+                <div className="flex items-center px-3 py-2 text-xs font-bold leading-snug text-white uppercase cursor-pointer hover:opacity-75">
+                  <Link href={"/event/detail/1"}>
+                    <span className="ml-2 text-2xl">เข้าห้อง</span>
+                  </Link>
+                </div>
               </li>
               <li className="nav-item">
                 <a
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Hello website</title>
+        <title>Maomun</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
