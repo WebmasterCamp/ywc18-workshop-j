a0c274b814ed1b01cd0a0256986335f70fc9d9bb
Mild-TN <21-mild-tn@users.noreply.git.innovasive.co.th>
2021-08-14 19:19:25 +0700

    Add blog page

---
+++ pages/blogs.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Blogs = () => {
+  // eslint-disable-next-line @next/next/no-img-element
+  return <img src="/blog_1.png" className="w-screen" />;
+};
+export default Blogs;
