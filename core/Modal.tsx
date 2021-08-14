68a5d2f7cdc6ee12ccae281f3a6d8c2780cb5c73
WhatTheFar <jakpat.m@gmail.com>
2021-08-14 18:55:18 +0700

    fix(modal): fix position

--- core/Modal.tsx
+++ core/Modal.tsx
@@ -1,27 +1,30 @@
 import React from "react";
+import { AiOutlineClose } from "react-icons/ai";
 
 export const Modal = ({ modalTitle, isShow, children, onClose }) => {
   return (
     <>
       {isShow ? (
         <>
-          <div className="fixed inset-0 z-50 flex items-center justify-center">
+          <div className="fixed top-0 left-0 right-0 bottom-0 z-50 flex items-center justify-center">
             <div className="relative mx-auto my-6">
               <div className="flex flex-col w-full bg-white border-0 outline-none focus:outline-none">
-                <div className="flex items-start justify-between p-5 border-b border-solid rounded-t border-blueGray-200">
-                  <h3 className="text-3xl font-semibold">{modalTitle}</h3>
+                <div className="flex justify-end">
                   <button
-                    className="float-right p-1 ml-auto text-3xl font-semibold leading-none text-black bg-transparent border-0 outline-none opacity-5 focus:outline-none"
+                    className="m-4 float-right text-3xl font-semibold leading-none text-black bg-transparent border-0 outline-none opacity-5 focus:outline-none"
                     onClick={onClose}
                   >
-                    <span className="text-xl text-black">X</span>
+                    <AiOutlineClose />
                   </button>
+                </div>
+                <div className="flex items-start justify-between p-5 border-b border-solid rounded-t border-blueGray-200">
+                  <h3 className="text-3xl font-semibold">{modalTitle}</h3>
                 </div>
                 {children}
               </div>
             </div>
           </div>
-          <div className="fixed inset-0 z-40 bg-black opacity-50"></div>
+          <div className="fixed top-0 left-0 right-0 bottom-0 z-40 bg-black opacity-50"></div>
         </>
       ) : null}
     </>
