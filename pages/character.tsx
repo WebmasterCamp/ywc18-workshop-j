e52515de7b0d6037bc4e1ffe2bfbf2e602edf176
WhatTheFar <jakpat.m@gmail.com>
2021-08-14 16:01:14 +0700

    feat(character): init character component

---
+++ pages/character.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import Character from "../src/character.tsx";
+
+const CharacterPage: NextPage = () => {
+  return <Character />;
+};
+
+export default CharacterPage;
---
+++ src/character.tsx
@@ -0,0 +1,30 @@
+import type { NextPage } from "next";
+import { BiLeftArrowCircle, BiRightArrowCircle } from "react-icons/bi";
+
+const characters = ["/phaser-logo.png"];
+
+const SelectCharacter: NextPage = () => {
+  return (
+    <div className="container bg-[#FFFFFF] flex flex-col items-center p-16">
+      <span className="text-4xl">เลือกตัวละคร</span>
+      <div className="pt-8 text-2xl">
+        <label htmlFor="fname" className="m-2">
+          ชื่อตัวละคร
+        </label>
+        <input type="text" id="fname" name="fname" />
+      </div>
+      <div className="flex items-center pt-8">
+        <BiLeftArrowCircle className="text-4xl" />
+        <div className="m-4 w-[128px] h-[128px]"></div>
+        <BiRightArrowCircle className="text-4xl" />
+      </div>
+      <div className="pt-8">
+        <button className="rounded-[4px] p-2 mx-4 bg-[#FFDADA]">
+          เข้าห้อง
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SelectCharacter;
