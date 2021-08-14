7dd80aed73aa9cb053b0f8dd54784a4de4411ed5
Mild-TN <21-mild-tn@users.noreply.git.innovasive.co.th>
2021-08-14 13:50:46 +0700

    Add carousel of Home page

---
+++ features/HomePage/components/Carousel.tsx
@@ -0,0 +1,71 @@
+import React, { useState, useRef, useEffect } from "react";
+import classNames from "classnames";
+import _ from "lodash";
+import { EventCard } from "./EventCard";
+
+interface CarouselProps {
+  data: any;
+}
+
+const delay = 3000;
+
+export const Carousel = ({ data }: CarouselProps) => {
+  const [index, setIndex] = useState(0);
+  const timeoutRef = useRef(null);
+
+  const resetTimeout = () => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef?.current);
+    }
+  };
+
+  useEffect(() => {
+    resetTimeout();
+    timeoutRef.current = setTimeout(
+      () =>
+        setIndex((prevIndex) =>
+          prevIndex == data.length - 1 ? 0 : prevIndex + 1
+        ),
+      delay
+    );
+
+    return () => {
+      resetTimeout();
+    };
+  }, [index]);
+
+  return (
+    <div className="relative max-w-full mx-auto overflow-hidden tablet:h-[550px]">
+      <div className="absolute bottom-0 z-20 flex justify-center w-full mb-10">
+        {_.map(data, (item, idx: number) => (
+          <div
+            key={idx}
+            className={classNames("h-4 w-4 mx-2 rounded-full bg-pink-200", {
+              "bg-pink-500": index === idx,
+            })}
+            onClick={() => {
+              setIndex(idx);
+            }}
+          ></div>
+        ))}
+      </div>
+
+      <div
+        className="z-0 space-x-6 transition duration-1000 ease-in whitespace-nowrap"
+        style={{ transform: `translate3d(${-index * (100 / 3)}%, 0, 0)` }}
+      >
+        {_.map(data, (item, i) => (
+          <div className="inline-block" key={i}>
+            <EventCard
+              description={item.description}
+              image={item.image}
+              tag={item.tag}
+              time={item.date}
+              eventName={item.event_name}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
